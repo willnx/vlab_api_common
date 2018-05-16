c3df493302747ea579e07829df1f99abbcaf88c9
willnx <willnx@laptop01.home>
2018-05-15 18:15:37 -0700

    added author email

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ from setuptools import setup, find_packages
 
 setup(name="vlab-api-common",
       author="Nicholas Willhite",
+      author_email="willnx84@gmail.com",
       version='2018.05.11',
       packages=find_packages(),
       include_package_data=True,
