ebf81a66deee6ce6d17e443eccf250e8ace94b4a
Nicholas Willhite <willhite.nicholas@gmail.com>
2018-05-30 17:40:07 -0700

    Bug cleanup, also fixed #4 (#5)
    * removed vmware.py
    
    * added long_description to setup.py, fixes #4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(name="vlab-api-common",
       author="Kevin Broadwater, Nicholas Willhite",
       author_email="willnx84@gmail.com",
-      version='2018.05.22',
+      version='2018.05.30',
       packages=find_packages(),
       include_package_data=True,
       classifiers=[
@@ -21,6 +21,7 @@ setup(name="vlab-api-common",
         'Programming Language :: Python :: 3.5',
       ],
       description="Common logic used by all the RESTful API services of vLab",
+      long_description=open('README.rst').read(),
       install_requires=['flask-classy', 'requests', 'pyjwt', 'ujson', 'jsonschema',
                         'cryptography', 'pyVmomi']
       )
